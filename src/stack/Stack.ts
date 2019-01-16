45e9bfb342f1ade63645adcd954697c33e36f1ad
dhf@acewill.cn <dhf@acewill.cn>
2019-01-16 10:46:26 +0800

    功能：完善栈单元测试

--- src/stack/Stack.ts
+++ src/stack/Stack.ts
@@ -1,68 +1,3 @@
-// export class Stack {
-//     private top:any;
-//     private size: number;
-//     constructor(){
-//         this.top = null;
-//         this.size = 0;
-//     }
-//     private push(data:any){
-//         let node: any = {
-//             data : data,
-//             next: null
-//         };
-//         node.next = this.top;
-//         this.top = node;
-//         this.size++;
-//     }
-//     private peek(){
-//         return this.top === null ? null : this.top.data;
-//     }
-//     private pop(){
-//         if(this.top === null) return null;
-//         let out = this.top;
-//         this.top = this.top.next;
-//         if(this.size>0)this.size--;
-//         return out.data;
-//     }
-//     private clear(){
-//         this.top = null;
-//         this.size = 0;
-//     }
-//     private isEmpty(){
-//         return this.top === null && this.size==0  ? true : false;
-//     }
-//     private getElem(i:number){
-//         if(typeof i != "number")return null;
-//         let p = this.top,
-//             j = this.size-1;
-//         while(p && j>i){
-//             p = p.next;
-//             j--;
-//         };
-//         return (!p || j<i) ? null : p.data;
-//     }
-//     private remove(cb:any):any{
-//         let p = this.top,
-//             j = this.size - 1,
-//             match = false,
-//             pre = null;
-//         while(p && !match){
-//             match = typeof cb == "function" ? cb(p) : (function(item){
-//                 return item.data == cb;
-//             })(p);
-//             if(!match) pre = p;
-//             p = p.next;
-//             j--;
-//         }
-//         if(!match) return null;
-//         if(j== this.size-2){
-//             this.pop();
-//         }else if(pre && pre.next){
-//             pre.next = pre.next.next;
-//             this.size--;
-//         };
-//     }
-// }
 import { Collection } from "../Collection";
 import LinkList from "../linklist/LinkList";
 import { LinkNode } from "../linklist/LinkNode";
@@ -89,7 +24,7 @@ export class Stack<T> extends Collection<LinkNode<T>> {
     }
 
     /**
-     * 看一看
+     * 查看栈顶元素
      */
     public peek(){
         if (!this.linkList.getTailNode()){
---
+++ src/stack/__test__/Stack.test.ts
@@ -0,0 +1,48 @@
+import { Stack } from "../Stack";
+
+describe("Stack test", () => {
+    test("push in stack", () => {
+        const stack = new Stack();
+        stack.push(1);
+        expect(stack.peek()).toBe(1);
+        stack.push(2);
+        expect(stack.peek()).toBe(2);
+    });
+    test("pop in stack", () => {
+        const stack = new Stack();
+        expect(stack.pop()).toBeNull();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+        expect(stack.peek()).toBe(2);
+        stack.pop();
+        expect(stack.peek()).toBe(1);
+        stack.pop();
+        expect(stack.isEmpty()).toEqual(true);
+    });
+    test("peek in stack", () => {
+        const stack = new Stack();
+        expect(stack.peek()).toBeNull();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        expect(stack.peek()).toBe(3);
+    });
+    test("isEmpty in stack", () => {
+        const stack = new Stack();
+        expect(stack.isEmpty()).toEqual(true);
+        stack.push(1);
+        expect(stack.isEmpty()).toEqual(false);
+    });
+    test("toString in stack", () => {
+        const stack = new Stack();
+        stack.push(1);
+        expect(stack.toString()).toEqual("1");
+    });
+    test("toArray in stack", () => {
+        const stack = new Stack();
+        stack.push(1);
+        expect(stack.toArray()).toEqual([{next: null, value: 1}]);
+    });
+});
